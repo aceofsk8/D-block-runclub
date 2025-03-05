005d45315e5d591fd5a9bd2fa8b7447191e1f825
aceofsk8 <you@example.com>
2025-03-05 15:14:43 -0700

    Renamed all files and folders to lowercase for case sensitivity fix

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///runs.db'
+import os
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///runs.db")
+
 db = SQLAlchemy(app)
 
 class Profile(db.Model):
