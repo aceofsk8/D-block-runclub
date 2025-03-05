950f58bdfa8109bfb92f0289e438ec4d633d3e41
aceofsk8 <you@example.com>
2025-03-05 15:23:20 -0700

    Auto-create database tables on startup

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ def view_profile(profile_id):
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        db.create_all()  # Creates tables if they don't exist
     app.run(debug=True)
